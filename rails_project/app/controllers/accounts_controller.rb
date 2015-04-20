efa389ef0d79f0c78226aa9aa1fad13db96d408b
Sophy Chen <sophychen2877@gmail.com>
2015-04-19 20:49:42 -0400

    asad change accounts controller

--- rails_project/app/controllers/accounts_controller.rb
+++ rails_project/app/controllers/accounts_controller.rb
@@ -1,90 +1,102 @@
 class AccountsController < ApplicationController
-
-  layout "logged_in", :except => [:sign_in, :new, :create_account, :process_sign_in]
-
-  def sign_in
-  end
-
-  def process_sign_in
-    upcombo = [params['uname'], params['pword']]
-    if (Account.exists?(:username => upcombo[0], :password => upcombo[1]))
-     accs = Account.all
-     @x = 0;
-     accs.each do |acc|
-       if (acc.username == upcombo[0] && acc.password == upcombo[1])
-         @x = acc.id
-       end
-     end
-     @login = true
-     redirect_to(:controller => 'users' , :action => 'show', :id => @x)
-    else
-      flash.now[:notice] = "Invalid Login"
-      render('sign_in')
+    
+    layout "logged_in", :except => [:sign_in, :new, :create_account, :process_sign_in]
+    
+    def sign_in
     end
-  end
-
-  def show
-    @account = Account.find(params[:id])
-  end
-
-  def create_account
-    @account = Account.new(account_params)
-    if (Account.exists?(:username => @account.username))
-      flash.now[:notice] = "That username is already being used"
-      render('new')
-    elsif (Account.exists?(:email => @account.email))
-      flash.now[:notice] = "That email is already being used"
-      render('new')
-    else
-       if @account.save
-        @account.user = User.new
-        @theID = @account.id
-        redirect_to(:controller => 'users' ,:action => 'index', :id => @account.id)
-      else
-        flash.now[:notice] = "An error occurred when trying to create your account"
-        render('new')
-      end
+    
+    def already_logged_in
+        @account = Account.find(params[:id])
     end
-  end
-
-  def new
-    @account = Account.new
-  end
-
-  def edit
-    @account = Account.find(params[:id])
-  end
-
-  def update
-    pword = [params[:oldpword], params[:newpword1], params[:newpword2]]
-    @account = Account.find(params[:id])
-    if (@account.password == pword[0] && pword[1] == pword[2])
-      if @account.update_attributes(:password => pword[1])
-        flash[:notice] = "Password successfully updated"
-        redirect_to(:controller => 'users', :action => 'show', :id => @account.id)
-      else
-        flash.now[:notice] = "Unable to update password"
-        render('edit')
-      end
-    else
-      flash.now[:notice] = "Your passwords do not match"
-      render('edit')
+    
+    def process_sign_in
+        upcombo = [params['uname'], params['pword']]
+        if (Account.exists?(:username => upcombo[0], :password => upcombo[1]))
+            accs = Account.all
+            @x = 0;
+            accs.each do |acc|
+                if (acc.username == upcombo[0] && acc.password == upcombo[1])
+                    @x = acc.id
+                end
+            end
+            @login = true
+            redirect_to(:controller => 'users' , :action => 'show', :id => @x)
+            else
+            flash.now[:notice] = "Invalid Login"
+            render('sign_in')
+        end
     end
-  end
-
-  def delete
-    @account = Account.find(params[:id])
-  end
-
-  def destroy
-    @account = Account.find(params[:id])
-    @account.destroy
-    redirect_to(:action => 'new')
-  end
-
-  private 
+    
+    def show
+        @account = Account.find(params[:id])
+    end
+    
+    def create_account
+        if(account_params[:username].length > 25 || account_params[:email].length > 25 || account_params[:password].length > 25)
+            flash.now[:notice] = "Input too long"
+            render('new')
+            elsif(account_params[:username] == "" || account_params[:email] == "" || account_params[:password] == "")
+            flash.now[:notice] = "You must have a valid username, email and password"
+            render('new')
+            else
+            @account = Account.new(account_params)
+            if (Account.exists?(:username => @account.username))
+                flash.now[:notice] = "That username is already being used"
+                render('new')
+                elsif (Account.exists?(:email => @account.email))
+                flash.now[:notice] = "That email is already being used"
+                render('new')
+                else
+                if @account.save
+                    @account.user = User.new
+                    @theID = @account.id
+                    redirect_to(:controller => 'users' ,:action => 'index', :id => @account.id)
+                    else
+                    flash.now[:notice] = "An error occurred when trying to create your account"
+                    render('new')
+                end
+            end
+        end
+    end
+    
+    def new
+        @account = Account.new
+    end
+    
+    def edit
+        @account = Account.find(params[:id])
+    end
+    
+    def update
+        pword = [params[:oldpword], params[:newpword1], params[:newpword2]]
+        @account = Account.find(params[:id])
+        if (@account.password == pword[0] && pword[1] == pword[2])
+            if @account.update_attributes(:password => pword[1])
+                flash[:notice] = "Password successfully updated"
+                redirect_to(:controller => 'users', :action => 'show', :id => @account.id)
+                else
+                flash.now[:notice] = "Unable to update password"
+                render('edit')
+            end
+            else
+            flash.now[:notice] = "Your passwords do not match"
+            render('edit')
+        end
+    end
+    
+    def delete
+        @account = Account.find(params[:id])
+    end
+    
+    def destroy
+        @account = Account.find(params[:id])
+        @account.destroy
+        redirect_to(:action => 'new')
+    end
+    
+    private 
     def account_params
-      params.require(:account).permit(:username, :email, :password)
+        params.require(:account).permit(:username, :email, :password)
     end
-
+    
 end
