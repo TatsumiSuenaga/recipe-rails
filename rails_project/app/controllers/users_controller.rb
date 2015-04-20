f122131c96eeeb4dbd3a07d76b92cc0c0bfcd823
Sophy Chen <sophychen2877@gmail.com>
2015-04-19 20:52:41 -0400

    asad edited user controller

--- rails_project/app/controllers/users_controller.rb
+++ rails_project/app/controllers/users_controller.rb
@@ -1,54 +1,47 @@
 class UsersController < ApplicationController
-  
-  layout 'logged_in'
-
-  def index
-   @users = User.order("user_id ASC")
-  end
-
-  def show  
-  @users = User.find(params[:id])
-  end
-  
-  def new
-	@users = User.new
-  end 
-  
-  def create
-	@users = User.new(user_params)
-      if @users.save
-        redirect_to(:action => 'index')
-    else
-        render('new')
+    
+    layout 'logged_in'
+    
+    def index
+        @users = User.order("user_id ASC")
     end
-    if @users.save
-         flash[:notice] = "Recipe created successfully."
-    end
-  end
-  
-  def edit 
-    @users = User.find(params[:id])
-  end
-
-  def update
+    
+    def show
         @users = User.find(params[:id])
-        if @users.update_attributes(user_params)
-	    flash[:notice] = "User updated successfully."
-            redirect_to(:action => 'show', :id => @users.id)
-        else
+    end
+    
+    def edit
+        @users = User.find(params[:id])
+    end
+    
+    def update
+        @users = User.find(params[:id])
+        if (user_params[:first_name].length > 25 || user_params[:last_name].length > 50 || user_params[:bio].length > 2000)
+            flash[:notice] = "Input too long"
+            render('edit')
+            elsif(user_params[:first_name] != "" && user_params[:last_name] != "" && user_params[:bio] != "")
+            if @users.update_attributes(user_params)
+                flash[:notice] = "Profile updated successfully."
+                redirect_to(:action => 'show', :id => @users.id)
+                else
+                flash[:notice] = "There was an error updating your profile"
+                render('edit')
+            end
+            else
+            flash[:notice] = "You must include a name or bio"
             render('edit')
         end
-           
-  end
-  def delete
-  end 
-
-  def destroy
-  end
-  
-private
-  def user_params
-      params.require(:user).permit(:first_name,:last_name, :bio, :email)
-  end
-
+    end
+    
+    def delete
+    end
+    
+    def destroy
+    end
+    
+    private
+    def user_params
+        params.require(:user).permit(:first_name,:last_name, :bio)
+    end
+    
 end
